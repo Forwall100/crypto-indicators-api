6957815879f193f5cc82603de934df8bb0aaa8a7
Forwall100 <hezeri546@gmail.com>
2022-08-19 16:36:23 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import re
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message"}
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        port=8000,
+        host="127.0.0.1",
+        reload=True
+    )
