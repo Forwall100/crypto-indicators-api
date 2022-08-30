75b0ab9b7d295a3905fb8344a771112d28eb1428
Forwall100 <hezeri546@gmail.com>
2022-08-30 09:51:43 +0700

    Add intervals dict in config

--- utils/sma.py
+++ utils/sma.py
@@ -1,15 +1,5 @@
 import requests
-
-intervals = {
-    "1m": 1,
-    "5m": 5,
-    "15m": 15,
-    "30m": 30,
-    "1h": 60,
-    "4h": 240,
-    "1d": 1440,
-    "1w": 10080
-}
+from config import intervals
 
 
 def sma(interval, candles, symbol):
