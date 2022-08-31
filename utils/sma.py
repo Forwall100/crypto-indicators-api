5cb637b8bd55458f6ca4928693f4b4e3c8405206
Forwall100 <hezeri546@gmail.com>
2022-08-31 17:01:13 +0700

    Calculation of SMA is made simpler

--- utils/sma.py
+++ utils/sma.py
@@ -6,13 +6,10 @@ def sma(interval, candles, symbol):
     url = f"https://api.kraken.com/0/public/OHLC?pair={symbol}&interval={intervals[interval]}"
     response = requests.get(url).json()
 
-    sum_close = 0
-    counter = 0
+    prices = []
     try:
-        for i in response["result"][list(response["result"].keys())[0]][::-1]:
-            counter += 1
-            if counter <= candles:
-                sum_close += float(i[4])
-        return (sum_close/candles)
+        for i in response["result"][list(response["result"].keys())[0]][::-1][:candles]:
+            prices.append(float(i[4]))
+        return (sum(prices)/candles)
     except:
         raise ValueError(response['error'])
