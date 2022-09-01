a3abb52382e39d2727cc20cb9442b746eb5b0a1a
Forwall100 <hezeri546@gmail.com>
2022-09-01 22:10:42 +0700

    Fix codestyle

--- utils/get_OHLC.py
+++ utils/get_OHLC.py
@@ -1,10 +1,11 @@
 import requests
 from .constans import intervals
 
+
 def kraken_OHLC(pair, interval):
     try:
         url = f"https://api.kraken.com/0/public/OHLC?pair={pair}&interval={intervals[interval]}"
         response = requests.get(url).json()
         return response["result"][list(response["result"].keys())[0]]
-    except:
+    except ValueError:
         raise ValueError("Unknown pair")
