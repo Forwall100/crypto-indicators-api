cb3e411ab7b74095c9e2eb2f929113bd6a25ca06
Forwall100 <hezeri546@gmail.com>
2022-09-01 21:49:28 +0700

    Add unknown pair exception

--- utils/get_OHLC.py
+++ utils/get_OHLC.py
@@ -2,6 +2,9 @@ import requests
 from .constans import intervals
 
 def kraken_OHLC(pair, interval):
-    url = f"https://api.kraken.com/0/public/OHLC?pair={pair}&interval={intervals[interval]}"
-    response = requests.get(url).json()
-    return response["result"][list(response["result"].keys())[0]]
+    try:
+        url = f"https://api.kraken.com/0/public/OHLC?pair={pair}&interval={intervals[interval]}"
+        response = requests.get(url).json()
+        return response["result"][list(response["result"].keys())[0]]
+    except:
+        raise ValueError("Unknown pair")
