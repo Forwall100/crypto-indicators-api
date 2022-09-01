14470a31df2f5222b0ff7c68bca1bfbd9269bf19
Forwall100 <hezeri546@gmail.com>
2022-09-01 22:13:36 +0700

    Fix bug for test action

--- utils/indicators/sma.py
+++ utils/indicators/sma.py
@@ -5,7 +5,7 @@ def sma(candles, OHLC):
         try:
             for i in OHLC[::-1][:candles]:
                 prices.append(float(i[4]))
-            return (sum(prices)*candles)
+            return (sum(prices)/candles)
         except ZeroDivisionError:
             raise ZeroDivisionError("The period cannot be equal to 0")
         except ValueError:
