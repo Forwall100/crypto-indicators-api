1a235cdf93431780814cd38e298ed01d766b40ed
Forwall100 <hezeri546@gmail.com>
2022-09-02 11:55:05 +0700

    Add some exceptions

--- utils/indicators/ema.py
+++ utils/indicators/ema.py
@@ -2,14 +2,17 @@ from statistics import mean
 
 
 def ema(candles, OHLC):
-    result = [mean([float(price[4]) for price in OHLC])]
-    k = 2/(candles+1)
-
-    try:
-        for price in OHLC:
-            price = float(price[4])
-            result.append((price-result[-1])*k+result[-1])
-
-        return (result[-1])
-    except:
-        raise ValueError(OHLC['error'])
+    if candles > 0:
+        try:
+            result = [mean([float(price[4]) for price in OHLC])]
+            k = 2/(candles+1)
+            for price in OHLC:
+                price = float(price[4])
+                result.append((price-result[-1])*k+result[-1])
+            return (round(result[-1], 2))
+        except ValueError:
+            raise ValueError("Unknown pair")
+    elif candles == 0:
+        raise ValueError("The period cannot be equal to 0")
+    elif candles < 0:
+        raise ValueError("The period cannot be negative")
