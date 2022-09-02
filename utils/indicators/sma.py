608140d844fb6aef861311eda698430fc75ee47f
Forwall100 <hezeri546@gmail.com>
2022-09-02 11:55:36 +0700

    Add rounding of the result

--- utils/indicators/sma.py
+++ utils/indicators/sma.py
@@ -5,7 +5,7 @@ def sma(candles, OHLC):
         try:
             for i in OHLC[::-1][:candles]:
                 prices.append(float(i[4]))
-            return (sum(prices)/candles)
+            return (round(sum(prices)/candles, 2))
         except ZeroDivisionError:
             raise ZeroDivisionError("The period cannot be equal to 0")
         except ValueError:
