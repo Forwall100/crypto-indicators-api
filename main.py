1e8c411ab87e0b7cee85abca2208e3905112e492
Forwall100 <hezeri546@gmail.com>
2022-09-03 17:00:14 +0700

    Add port and host to constans.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 import uvicorn
 from endpoints import sma, ema, rsi
+from utils.constans import HOST, PORT
 
 app = FastAPI()
 
@@ -11,7 +12,7 @@ app.include_router(rsi.router)
 if __name__ == "__main__":
     uvicorn.run(
         "main:app",
-        port=8000,
-        host="127.0.0.1",
+        port=PORT,
+        host=HOST,
         reload=True
     )
