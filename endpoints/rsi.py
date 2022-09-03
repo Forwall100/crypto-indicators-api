c7bc9af8ac47fe1143a9e0aad8d96de289b629f6
Forwall100 <hezeri546@gmail.com>
2022-09-03 11:19:17 +0700

    Add some exceptions for RSI

--- endpoints/rsi.py
+++ endpoints/rsi.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Query
+from fastapi import APIRouter, Query, HTTPException
 from models.schemas import Interval
 from utils.indicators.rsi import rsi
 from utils.get_OHLC import kraken_OHLC
@@ -9,5 +9,5 @@ router = APIRouter()
 async def get_rsi(interval: Interval, symbol: str, candles: int = Query(default=14)):
     try:
         return {"value": rsi(candles, kraken_OHLC(symbol, interval))}
-    except ValueError:
-        return {"Unknown pair"}
+    except ValueError as e:
+        raise HTTPException(status_code=422, detail=str(e))
--- utils/indicators/rsi.py
+++ utils/indicators/rsi.py
@@ -2,19 +2,25 @@ from statistics import mean
 
 
 def rsi(candles, OHLC):
-
     green = []
     red = []
 
-    try:
-        for i in OHLC[::-1][:candles]:
-            open = float(i[1])
-            close = float(i[4])
-            if open > close:
-                red.append(open-close)
-            else:
-                green.append(close-open)
-        result = 100 - 100/(1 + mean(green)/mean(red))
-        return result
-    except:
-        raise ValueError(OHLC['error'])
+    if candles > 1:
+        try:
+            for i in OHLC[::-1][:candles]:
+                open = float(i[1])
+                close = float(i[4])
+                if open > close:
+                    red.append(open-close)
+                else:
+                    green.append(close-open)
+            result = 100 - 100/(1 + mean(green)/mean(red))
+            return round(result, 2)
+        except ValueError:
+            raise ValueError("Unkown pair")
+    elif candles == 1:
+        raise ValueError("The period cannot be equal to 1")
+    elif candles == 0:
+        raise ValueError("The period cannot be equal to 0")
+    elif candles < 0:
+        raise ValueError("The period cannot be negative")
