0683322e8b4e1e7195347ef8e0727f7c5f3a51b0
Forwall100 <hezeri546@gmail.com>
2022-09-09 12:28:46 +0700

    Fix extreme values

--- utils/indicators/rsi.py
+++ utils/indicators/rsi.py
@@ -1,11 +1,12 @@
 from statistics import mean
+from unittest import result
 
 
 def rsi(candles, OHLC):
     green = []
     red = []
 
-    if candles > 1:
+    if candles > 0:
         try:
             for i in OHLC[::-1][:candles]:
                 open = float(i[1])
@@ -14,12 +15,15 @@ def rsi(candles, OHLC):
                     red.append(open-close)
                 else:
                     green.append(close-open)
-            result = 100 - 100/(1 + mean(green)/mean(red))
+            if len(green) == 0:
+                result = 0
+            elif len(red) == 0:
+                result = 100
+            else:
+                result = 100 - 100/(1 + mean(green)/mean(red))
             return round(result, 2)
         except ValueError:
-            raise ValueError("Unkown pair")
-    elif candles == 1:
-        raise ValueError("The period cannot be equal to 1")
+            raise ValueError("Unknown pair")
     elif candles == 0:
         raise ValueError("The period cannot be equal to 0")
     elif candles < 0:
